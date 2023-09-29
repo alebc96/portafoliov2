43c7ec85f1e5c8c9ace6636b071f667af9a7c184
alebc96 <alebecerrac96@gmail.com>
2023-09-29 16:02:58 +0200

    update projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -4,11 +4,26 @@ import libraryImg from '../../assets/libreria.png'
 import comprasImg from '../../assets/comprass.png'
 import ecommerceMUI from '../../assets/ecommerce01.png'
 import ecommerceBW from '../../assets/ecommerce.png'
+import shopMin from '../../assets/shop-min.png'
 import passwordGen from '../../assets/password-generator-min.png'
 import ProjectCard from "../project-card/ProjectCard"
 import Project from '../../interfaces/project'
 
 const projects: Project[] = [
+  {
+    image: passwordGen,
+    name: 'Password generator',
+    link: 'https://tailwind-pass-generator.netlify.app',
+    description: "Password generator is a challenge from FrontendMentor where you need to implement a form with certain parameters to create a secure password. You can choose from different types of characters, and the application will recommend, according to a defined criteria, whether it is secure or not.",
+    tecs: ['ReactJS', 'Tailwind', 'TSX', 'TypeScript']
+  },
+  {
+    image: shopMin,
+    name: 'Dashboard Food ecommerce',
+    link: 'https://react-dashboard-ecommerce.netlify.app',
+    description: "Shop is a food e-commerce design where you can order food for delivery, view orders and available offers in the form of a dashboard. It features a shopping cart with the desired order, descriptions, and other details. It is a responsive application, so it has a design that adapts to both mobile devices and desktop.",
+    tecs: ['ReactJS', 'Tailwind', 'TSX', 'TypeScript']
+  },
   {
     image: libraryImg,
     name: 'Library',
@@ -44,13 +59,7 @@ const projects: Project[] = [
     description: 'ComprasApp is a web service developed with the aim of creating a user account by registering on Firebase and generating lists of products to buy for a week. Users can add new lists and add products to them, which can be deleted or modified. The application was built using AngularJS, and for the server, NodeJs was used with a non-relational MongoDB database hosted on Atlas. It is a simple CRUD (Create, Read, Update, Delete) application with basic functionalities of POST, GET, DELETE, PUT, created with the purpose of learning and practice.',
     tecs: ['AngularJs', 'CSS', 'HTML', 'TypeScript', 'Bootstrap', 'Firebase', 'NodeJS', 'MongoDB']
   },
-  {
-    image: passwordGen,
-    name: 'Password generator',
-    link: 'https://tailwind-pass-generator.netlify.app',
-    description: "Password generator is a challenge from FrontendMentor where you need to implement a form with certain parameters to create a secure password. You can choose from different types of characters, and the application will recommend, according to a defined criteria, whether it is secure or not.",
-    tecs: ['ReactJS', 'Tailwind', 'TSX', 'TypeScript']
-  }
+  
 ]
 
 const Projects = () => {
